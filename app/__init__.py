cddfe9f63c5143e53b8e42580d2634e433aee7e6
Princewill <princewill.onyekah@kibo.school>
2024-02-01 16:23:36 +0100

    worked on image upload and handled expired tokens

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,17 @@ from .routes.auth_routes import auth_routes
 from .routes.user_routes import user_routes
 from .routes.post_routes import post_routes
 from .middlewares import setup_middlewares
-
+from werkzeug.utils import secure_filename
 
 def create_app(config_class=Config):
     """Create the Flask app instance"""
     app = Flask(__name__)
     app.config["JWT_SECRET_KEY"] = config_class.SECRET_KEY
+    UPLOAD_FOLDER = '/static/uploads'
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
     JWTManager(app)
     setup_middlewares(app)
 
